68c019691e007057b708988268b7701e2938dd7c
junidevs <52135894+junidevs@users.noreply.github.com>
2021-01-12 15:21:03 +0100

    Add files via upload
    Updated reload windows after clicked on Spoofieee

---
+++ Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () =>
+{
+return(
+    <div>
+         <div className="content-header">
+                         <h1 onClick={()=>window.location.reload()} className="title-header">
+                             Spoofieee
+                         </h1>
+                </div>
+     </div>
+);
+}
+export default Header;
