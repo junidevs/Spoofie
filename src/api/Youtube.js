f18ff3661e7132db494ec5f04811eae26bf20207
junidevs <52135894+junidevs@users.noreply.github.com>
2021-01-20 18:08:31 +0100

    Update Youtube.js

--- src/api/Youtube.js
+++ src/api/Youtube.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const KEY ='AIzaSyBZgY-Ck7YZIAOQxQNs4FqR6PxrGtBthP8';
+const KEY ='AIzaSyBVbFIW6lQOdVVbqPremjvWLi8FOOPAZ_w';
 
 export default axios.create({
     baseURL:'https://www.googleapis.com/youtube/v3',
